5f1dd105ebe3dde4901a983f483975d9b962b456
giriprakash <85009018+spider-251@users.noreply.github.com>
2022-07-21 19:42:31 +0530

    initial commit

---
+++ spider.sql
@@ -0,0 +1,7 @@
+INSERT INTO `untitled_name` (`id`, `country_name`, `country_name_eng`, `country_code`, `id`, `city_name`, `lat`, `lon`, `country_id`) VALUES
+(5, 'Polska', 'Poland', 'POL', 1, 'Warsaw', 52.237049, 21.017532, 5),
+(1, 'Deutschland', 'Germany', 'DEU', 2, 'Berlin', 52.520008, 13.404954, 1),
+(2, 'Srbija', 'Serbia', 'SRB', 3, 'Belgrade', 44.787197, 20.457273, 2),
+(3, 'Hrvatska', 'Croatia', 'HRV', 4, 'Zagreb', 45.815399, 15.966568, 3),
+(4, 'United Stated of America', 'United Stated of America', 'USA', 5, 'New York', 40.730610, -73.935242, 4),
+(4, 'United Stated of America', 'United Stated of America', 'USA', 6, 'Los Angeles', 34.052235, -118.243683, 4);
